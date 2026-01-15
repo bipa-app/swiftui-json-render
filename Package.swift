ee75a364aaa87da67616feeaf6f9daecc30e19ec
Luiz Parreira <luizfilipester@gmail.com>
2026-01-15 01:26:32 -0300

    Fix swift-testing version to 0.10.0 for Swift 5.9 compatibility

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-testing.git", from: "0.12.0")
+    .package(url: "https://github.com/apple/swift-testing.git", exact: "0.10.0")
   ],
   targets: [
     .target(
